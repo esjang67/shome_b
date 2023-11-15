42ae5eb709c4707bfe16ac425ab9beefabce65f9
esjang67 <esj67@naver.com>
2023-11-15 09:29:44 +0900

    front server add edit

--- src/main/java/com/esjang/sthome/config/WebConfig.java
+++ src/main/java/com/esjang/sthome/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
+		.allowedOrigins("https://localhost:3000")
 		.allowedOrigins("https://sweethome-24e1b.web.app")	// react 서버(배포후 변경해야함)
 		.allowedMethods("GET","POST","DELETE","PUT");		// get, post만 기본적으로 허용되어있므로 재설정함
 	}
